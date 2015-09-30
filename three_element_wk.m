ded0e582d1eb13b0f4323c0ccb0522efeaebf9b5
mammothb <mammothb+git@gmail.com>
2015-09-30 20:08:17 +0800

    adjusted parameters

--- three_element_wk.m
+++ three_element_wk.m
@@ -8,20 +8,25 @@ options = odeset('Refine', 8);
 % Defining modeling parameters for Windkessel Model
 % parameters for 3 element
 R1_3= 0.05;  % mmHg*sec/cm^3, characteristic impedance of aorta
-R2_3 = 0.9278;  % mmHg*sec/cm^3, systemic peripheral resistance
-C_3 = 1.47;  % cm^3/mmHg, systemic arterial compliance
+R2_3 = 1.1;  % mmHg*sec/cm^3, systemic peripheral resistance
+C_3 = 1.37;  % cm^3/mmHg, systemic arterial compliance
 % parameters for 4 element
-R1_4 = 0.071;  % mmHg*sec/cm^3, characteristic impedance of aorta
-R2_4 = 0.8495;  % mmHg*sec/cm^3, systemic peripheral resistance
-C_4 = 1.47;  % cm^3/mmHg, systemic arterial compliance
-L_4 = 5.2e-4;  % mmHs*s^2/cm^3, inertance of blood flow
+R1_4a = 0.065;  % mmHg*sec/cm^3, characteristic impedance of aorta
+R2_4a = 1.025;  % mmHg*sec/cm^3, systemic peripheral resistance
+C_4a = 1.47;  % cm^3/mmHg, systemic arterial compliance
+L_4a = 5.1e-4;  % mmHs*s^2/cm^3, inertance of blood flow
+% parameters for 4 element (RL parallel)
+R1_4b = 0.090;  % mmHg*sec/cm^3, characteristic impedance of aorta
+R2_4b = 0.9;  % mmHg*sec/cm^3, systemic peripheral resistance
+C_4b = 1.44;  % cm^3/mmHg, systemic arterial compliance
+L_4b = 0.018;  % mmHs*s^2/cm^3, inertance of blood flow
 %% Assumptions
 Tc = 60 / 72;  % 72 beats per second
 Ts = (2 / 5) * Tc;  % systole period
 cycle = 1;  % number of cardiac cycles for which WM is analysed
 % Modelling blood flow to the aorta
 syms ti q
-Qmax = solve(90 - int(q * (sin(pi * ti / Ts)), ti, 0, Ts), q);
+Qmax = solve(70 - int(q * (sin(pi * ti / Ts)), ti, 0, Ts), q);
 Q0 = eval(Qmax);
 sine = @(t) sin(pi * t / Ts);
 cosine = @(t) cos(pi * t / Ts);
@@ -29,8 +34,9 @@ Q = @(t) Q0 * sine(t) .* (t <= Ts); % for one cycle
 figure(1);
 % Analysis over 'cycle' number of cardiac cycles
 % Initial conditions all models
-P_ss = 80;
+P_ss = 79.9;
 P_ss2 = 80;
+P_ss3 = 80;
 for n = 1:cycle
   t = (n - 1) * Tc : 0.01 : n * Tc;
   % Blood flow for each cardiac cycle
@@ -46,10 +52,17 @@ for n = 1:cycle
   P_ss = P_m(end);
 
   %% Numerical solution for 4 Element WM
-  dydt2 = @(t, y) (-y / (R2_4 * C_4) + Q(t) * (R2_4 + R1_4) / (R2_4 * C_4) +...
-      (R1_4 + L_4 / C_4 / R2_4) * dQdt(t) + L_4 * d2Qdt2(t));
+  dydt2 = @(t, y) (-y / (R2_4a * C_4a) + Q(t) * (R2_4a + R1_4a) / (R2_4a *...
+      C_4a) + (R1_4a + L_4a / C_4a / R2_4a) * dQdt(t) + L_4a * d2Qdt2(t));
   [t_m2, P_m2] = ode113(dydt2, [(n - 1) * Tc; n * Tc], P_ss2, options);
   P_ss2 = P_m2(end);
+
+  %% Numerical solution for 4 element WM (LR parallel)
+  d2ydt2 = @(t, y) [y(2); R1_4b * d2Qdt2(t) + (R1_4b / C_4b / R2_4b +...
+      R1_4b / C_4b) * dQdt(t) + R1_4b / C_4b / L_4b * Q(t) - (1 / C_4b /...
+      R2_4b + R1_4b / L_4b) * y(2) - R1_4b / C_4b / L_4b / R2_4b * y(1)];
+  [t_m3, P_m3] = ode45(d2ydt2, [(n - 1) * Tc; n * Tc], [P_ss2; 0], options);
+  P_ss3 = P_m3(end, 1);
 
   % Storing results in a single vector
   if (n == 1)
@@ -57,24 +70,32 @@ for n = 1:cycle
     T = t_m;
     P2 = P_m2;
     T2 = t_m2;
+    P3 = P_m3(:, 1);
+    T3 = t_m3;
   else
     P = [P; P_m];
     T = [T; t_m];
     P2 = [P2; P_m2];
     T2 = [T2; t_m2];
+    P3 = [P3; P_m3(:, 1)];
+    T3 = [T3; t_m3];
   end
 end
 
 plot(T, P, 'LineWidth', 2);
 hold on;
 plot(T2, P2, 'LineWidth', 2);
-disp(P_ss2);
+% hold on;
+% plot(T3, P3, 'LineWidth', 2);
+disp(P_ss);
+disp(max(P));
 ylim([0, 150]);
 xlim([0, n * Tc]);
 title('Aortic Blood Pressure (Numerical 3 Element WM)');
 ylabel('Pressure (mmHg)');
 xlabel('time (s)');
-% % Write to .dat file
-% dlmwrite('wk.dat', [T P], 'delimiter', ' ');
-% dlmwrite('wk1.dat', [t_m P_m], 'delimiter', ' ');
-% xlswrite('wk.xlsx', [T P]);
+% Write to .dat file
+dlmwrite('wk.dat', [T P], 'delimiter', ' ');
+dlmwrite('wk1.dat', [t_m P_m], 'delimiter', ' ');
+dlmwrite('wk2.dat', [t_m2 P_m2], 'delimiter', ' ');
+xlswrite('wk.xlsx', [T P]);
